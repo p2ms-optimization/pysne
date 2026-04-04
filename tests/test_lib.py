72bb9cce939493a2e2161aba02203ec62c4dd32d
AzzBen <azaryaben@gmail.com>
2026-04-04 15:59:06 +0700

    rename dynamic.py and test_lib import

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -4,10 +4,10 @@ import sys
 from typing import Dict, List, Tuple, Any
 
 from problems.benchmarks import get_problem_set
-from utils import objective_function, validate_solutions
-from clustering.dynamic import perform_iterative_clustering
-from optimizers import run_sdoa_on_clusters, select_final_roots
-from solver import solve_system
+from pysne.utils import objective_function, validate_solutions
+from pysne.clustering.clustering_process import perform_iterative_clustering
+from pysne.optimizers import run_sdoa_on_clusters, select_final_roots
+from pysne.solver import solve_system
 
 
 
