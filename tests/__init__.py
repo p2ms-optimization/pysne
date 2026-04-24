b960b5260b1544b9d336ed4349e56605667514c7
AzzBen <azaryaben@gmail.com>
2026-04-24 10:49:03 +0700

    update import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-from pysne.problems.benchmarks import get_problem_set
+from pysne.problems.benchmarks_sne import get_problem_set
 from pysne.utils import objective_function, validate_solutions
 from pysne.clustering.clustering_process import perform_iterative_clustering
 # from pysne.optimizers import , select_final_roots
